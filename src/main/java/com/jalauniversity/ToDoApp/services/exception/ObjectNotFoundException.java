07215d709ce1633c5371105902f4e7811b2740c5
AnxeLOo <vitoor.barbosa2002@gmail.com>
2024-04-17 14:49:24 -0300

    Obtendo um User por id

---
+++ src/main/java/com/jalauniversity/ToDoApp/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.jalauniversity.ToDoApp.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
