62879102771645bb95a8900d461da1cbdedd3ec7
AnxeLOo <vitoor.barbosa2002@gmail.com>
2024-04-17 13:24:39 -0300

    Conectando ao MongoDb com Repository e service

--- src/main/java/com/jalauniversity/ToDoApp/resources/UserResource.java
+++ src/main/java/com/jalauniversity/ToDoApp/resources/UserResource.java
@@ -4,26 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jalauniversity.ToDoApp.domain.User;
+import com.jalauniversity.ToDoApp.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Fernanda", "maria@jala.university");
-        User alex = new User("2", "Alex Silva", "alex@jala.university");
-        User pedro = new User("3", "Pedro Cavalcante", "pedro@jala.university");
-
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex, pedro));
-
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
