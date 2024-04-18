c14961e94186735b14f4ed54b85bef666edc6991
AnxeLOo <vitoor.barbosa2002@gmail.com>
2024-04-18 00:35:12 -0300

    Consulta simples com query methods

--- src/main/java/com/jalauniversity/ToDoApp/resources/TaskResource.java
+++ src/main/java/com/jalauniversity/ToDoApp/resources/TaskResource.java
@@ -1,13 +1,17 @@
 package com.jalauniversity.ToDoApp.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jalauniversity.ToDoApp.domain.Task;
+import com.jalauniversity.ToDoApp.repository.util.URL;
 import com.jalauniversity.ToDoApp.services.TaskService;
 
 @RestController
@@ -21,5 +25,12 @@ public class TaskResource {
     public ResponseEntity<Task> findById(@PathVariable String id) {
         Task obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    
+    @RequestMapping(value = "/titleseach", method = RequestMethod.GET)
+    public ResponseEntity<List<Task>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Task> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/jalauniversity/ToDoApp/services/TaskService.java
+++ src/main/java/com/jalauniversity/ToDoApp/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.jalauniversity.ToDoApp.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TaskService {
     public Task findById(String id) {
         Optional<Task> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Task> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
